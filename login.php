8d88e6b50aa512648e3698faac54a52c05f0e9d9
Elis Frroku <39886477+efrr@users.noreply.github.com>
2019-04-08 16:45:16 -0600

    Update login.php

--- login.php
+++ login.php
@@ -1,45 +1,28 @@
 <?php
+	$servername = "localhost";
+	$username = "project";
+	$password = "databases";
+	$dbname = "finalProject";
 
-	$host="localhost"; // Host name 
-	$username="project"; // Mysql username 
-	$password="databases"; // Mysql password 
-	$db_name="finalProject"; // Database name 
-	$tbl_name="customers"; // Table name 
+	$_uname=$_POST['uname'];
+	$_pass=$_POST['pass']; 
 
-	// Connect to server and select databse.
-	@mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
-	mysql_select_db("$db_name")or die("cannot select DB");
+	$conn = new mysqli($servername, $username, $password, $dbname);
 
-	// username and password sent from form 
-	$username=$_POST['username']; 
-	$password=$_POST['password']; 
-
-	// To protect MySQL injection (more detail about MySQL injection)
-	$username = stripslashes($username);
-	$password = stripslashes($password);
-	$username = mysql_real_escape_string($username);
-	$password = mysql_real_escape_string($password);
-	$sql="SELECT * FROM $tbl_name WHERE email_address='$username' and password='$password'";
-	$result=mysql_query($sql);
-
-	// Mysql_num_row is counting table row
-	$count=mysql_num_rows($result);
-
-	// If result matched $username and $password, table row must be 1 row
-	if($count==1){
-		session_start();
-		$_SESSION['loggedin'] = true;
-		$_SESSION['username'] = $username;
-		
-		$_SESSION['id'] = $username;
-		$_SESSION['username'] = $username;
-		$_SESSION['verified'] = false;
-		$_SESSION['message'] = 'You are logged in!';
-		$_SESSION['type'] = 'alert-success';
-		header('Location: shoppingCart.html');
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
 	}
-	else
-	{
-		header('Location: login.html');
+
+	$sql = "SELECT * FROM customers WHERE username='$_uname' and password='$_pass'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+		$_SESSION['id'] = $_uname;
+        $_SESSION['username'] = $_uname;
+        header('Location: items.html');
+    }
+    else
+    {
+		header('Location; login.html');
 	}
+ 
 ?>
