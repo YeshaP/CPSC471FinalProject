f6cfdfb9bf352d20b480ca821b520a1703df38ee
Elis Frroku <39886477+efrr@users.noreply.github.com>
2019-04-09 18:01:42 -0600

    Update paymentPage.php

--- paymentPage.php
+++ paymentPage.php
@@ -15,12 +15,25 @@
 	
 	$card_number=$_POST['cardNumber'];
 	$card_type=$_POST['cardType'];
+	$expiry_year=$_POST['expiry_year'];
+	$expiry_month=$_POST['expiry_month'];
 
 
 	$uname=$_SESSION['username'];
 	
-	$sql = "INSERT INTO purchase VALUES('$card_number','$card_type','$uname')";
+	$sql = "INSERT INTO purchase VALUES('$card_number','$card_type', '$uname', '$expiry_month', '$expiry_year')";
 	if ($conn->multi_query($sql) === TRUE) {
+		
+		$_SESSION = array();
+		//Kill session cookie
+		if (ini_get("session.use_cookies")) {
+			$params = session_get_cookie_params();
+			setcookie(session_name(), '', time() - 42000,
+				$params["path"], $params["domain"],
+				$params["secure"], $params["httponly"]
+			);
+		}
+		session_destroy();
 		header('Location: index.html');
 	} else {
 		echo "Error: " . $sql . "<br>" . $conn->error;
