c2dc28073557112a8134616091ad5b9db82178f0
Elis Frroku <39886477+efrr@users.noreply.github.com>
2019-04-10 14:21:41 -0600

    Update shoppingCart.php

--- shoppingCart.php
+++ shoppingCart.php
@@ -5,23 +5,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <link rel="stylesheet" href="shoppingCart.css">
+  <link rel="stylesheet" href="items.css">
 </head>
 <body>
 
 <h1>Shopping Cart</h1>
 
-<div class="buyNow">
-  <button type="button" id="button1" onclick="window.location.href = 'confirmpurchase.php';">Buy Now</button>
-  <!-- <button onclick="myFunction()"> Buy Now </button> -->
+<div class="cart" align="right">
   <?php
-		if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)
+  
+		if ($_SESSION['loggedin'] == true)
 		{
-			echo "<a href='logout.php' class='button'>Logout</a>";
+			echo "<a href=\"logout.php\" class=\"button\">Logout</a>";
+			echo "<a href=\"confirmpurchase.php\" class=\"button\">Purchase</a>";
 		}
   ?>
 </div>
-<div>
+
 <?php
 	
 		$servername = "localhost";
@@ -43,17 +43,29 @@
 		$result = mysqli_query($conn, $sql);
 		
 		$cnt = mysqli_num_rows($result);
-	
+		//username, barcode, quantity, url
 		//If the user doesn't have any items in his cart, don't perform the while loop; otherwise, do perform it
 		if ($cnt > 0)
 		{
 			while($row = mysqli_fetch_assoc($result))
 			{
-				echo "<img src='$row[url]' height='500px' width='400px' border-style='solid' align='left'/>";
+				echo "<div style=\"border-style: dotted; display: inline-block; background-color: white; padding: 0;margin-top: 20px;margin-left: 10px;margin-right: 2px;margin-bottom: 20px;text-align:center;\">
+				</center><img src='$row[url]' height='300px' width='200px' border-style='solid'/>
+				<br/>
+				<h5 style=\"background-color: #white;display: inline-block;text-align:center; margin-bottom:5px\"> 
+				<p style=\"color:black; background-color: #white\" >
+					Quantity: $row[quantity]
+					<br> Barcode: $row[barcode]
+					</h5><br><br>
+				<form method=\"POST\" action=\"update.php\">
+					<input type=\"number\" name=\"quanti\" value=\"$row[quantity]\" />
+					<input type=\"hidden\" value=\"$row[barcode]\" name=\"var\" />
+					<input type=\"submit\" value=\"Add to cart\" />
+				</form>
+				</div>";
 			}
 		}
 	?>
-</div>
 <script>
   function myFunction(){
     confirm("Purchase successful!");
