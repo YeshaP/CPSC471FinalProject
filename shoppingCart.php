c14f6cb33cddc87f9f2f566b92fe5f4942f20984
Elis Frroku <39886477+efrr@users.noreply.github.com>
2019-04-10 21:34:45 -0600

    Update shoppingCart.php

--- shoppingCart.php
+++ shoppingCart.php
@@ -14,14 +14,38 @@
 <div class="cart" align="right">
   <?php
   
+		$servername = "localhost";
+		$username = "project";
+		$password = "databases";
+		$dbname = "finalProject";
+		
+		$conn = new mysqli($servername, $username, $password, $dbname);
+	
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		}
+		
+		$uname = $_SESSION['username'];
+  
 		if ($_SESSION['loggedin'] == true)
 		{
 			echo "<a href=\"logout.php\" class=\"button\">Logout</a>";
 			
 			//if count(shoppingcart with username='' <> 0)
-			echo "<a href=\"confirmpurchase.php\" class=\"button\">Purchase</a>";
+			
+			$sql = "SELECT * FROM shopping_cart WHERE username='$uname'";
+			$result = mysqli_query($conn, $sql);
+			
+			$count = mysqli_num_rows($result);
+			
+			if ($count <> 0)
+			{
+				echo "<a href=\"confirmpurchase.php\" class=\"button\">Purchase</a>";
+			}
 		}
   ?>
+  
+  <a href="items.php" class="button">Items</a>
 </div>
 
 <?php
@@ -39,7 +63,7 @@
 		
 		$uname = $_SESSION['username'];
 		
-		$RunningTotal = 0;
+		$_RUNNINGTOTAL = 0;
 		
 		$sql = "SELECT * FROM shopping_cart WHERE username='$uname'";
 		$result = mysqli_query($conn, $sql);
@@ -51,12 +75,21 @@
 		{
 			while($row = mysqli_fetch_assoc($result))
 			{
+				$sql2 = "SELECT * FROM items WHERE barcode='$row[barcode]'";
+				$resulting = mysqli_query($conn, $sql2);
+				
+				$temp = mysqli_fetch_assoc($resulting);
+				
+				$_RUNNINGTOTAL += ($temp[price] * 1.05);
+				
 				echo "<div style=\"border-style: dotted; display: inline-block; background-color: white; padding: 0;margin-top: 20px;margin-left: 10px;margin-right: 2px;margin-bottom: 20px;text-align:center;\">
 			</center><img src='$row[url]' height='300px' width='200px' border-style='solid'/>
 				<br/>
 				<h5 style=\"background-color: white;display: inline-block;text-align:center; margin-bottom:5px\"> 
-				<p style=\"color:black; background-color: #white\" >
-					Quantity: $row[quantity]
+				<p style=\"color:black; background-color: white\" >
+					Price: \$$temp[price]<br>
+					Quantity: $row[quantity]<br>
+					Inventory: $temp[quantity]
 					<br> Barcode: $row[barcode]
 					</h5><br><br>
 				<form method=\"POST\" action=\"update.php\">
@@ -66,8 +99,11 @@
 				</form>
 				</div>";
 			}
+			
+			$_SESSION['RunningTotal'] = $_RUNNINGTOTAL;
 		}
 	?>
+	
 <script>
   function myFunction(){
     confirm("Purchase successful!");
