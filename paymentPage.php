70bc7db4bc399586ba0336d2100948f591754187
Elis Frroku <39886477+efrr@users.noreply.github.com>
2019-04-10 21:35:05 -0600

    Update paymentPage.php

--- paymentPage.php
+++ paymentPage.php
@@ -1,42 +1,67 @@
 <?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="login.css">
+</head>
+<body>
+
+  <h1>User Registration</h1>
+  
+  <?php
+  
 	$servername = "localhost";
 	$username = "project";
 	$password = "databases";
 	$dbname = "finalProject";
-
-	// Create connection
+	
+	$uname=$_SESSION['username'];
+	
 	$conn = new mysqli($servername, $username, $password, $dbname);
 	// Check connection
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
 	}
-
-	session_start();
 	
-	$card_number=$_POST['cardNumber'];
-	$card_type=$_POST['cardType'];
-	$expiry_year=$_POST['expiry_year'];
-	$expiry_month=$_POST['expiry_month'];
-
-
-	$uname=$_SESSION['username'];
+	$sql = "SELECT * FROM customers WHERE username='$uname'";
+	$result = mysqli_query($conn, $sql);
+	$temp = mysqli_fetch_assoc($result);
 	
-	$sql = "INSERT INTO purchase VALUES('$card_number','$card_type', '$uname', '$expiry_month', '$expiry_year')";
-	if ($conn->multi_query($sql) === TRUE) {
-		
-		$_SESSION = array();
-		//Kill session cookie
-		if (ini_get("session.use_cookies")) {
-			$params = session_get_cookie_params();
-			setcookie(session_name(), '', time() - 42000,
-				$params["path"], $params["domain"],
-				$params["secure"], $params["httponly"]
-			);
-		}
-		session_destroy();
-		header('Location: index.html');
-	} else {
-		echo "Error: " . $sql . "<br>" . $conn->error;
-	}
-	$conn->close();
-?>
+	$RunningTotal = $_SESSION['RunningTotal'];
+  
+	echo "<div style=\"border-style: solid\"><h5>&nbsp;Your order has a total of \$$RunningTotal</h5>
+	
+	<p>&nbsp;Ship to: <br>&nbsp;$temp[street_name] , $temp[city] , $temp[province] , $temp[country] , $temp[postal_code]</p>
+	</div><br>";
+  
+  
+  ?>
+  <form action="processPayment.php" method="POST">
+    <!-- <div class="makePayment"> -->
+      <label for="cardNumber"><b>Card Number</b></label>
+      <input type="text" placeholder="Please enter Card Number" name="cardNumber" maxlength="16" required>
+      
+      <label for="expiry_month"><b>Expiry Month</b></label>
+      <input type="number"  placeholder="mm" name="expiry_month" maxlength="2" required>   
+      
+      <label for="expiry_year"><b>Expiry Year</b></label>
+      <input type="number" placeholder="yy" name="expiry_year" maxlength="2" required>  
+    
+    <select name="cardType">
+  		<option value="VISA">VISA</option>
+  		<option value="MasterCard">MasterCard</option>
+  		<option value="American_Express">American Express</option>
+  		<option value="Debit">Debit</option>
+	</select>
+
+
+    <button type="submit"><input type="submit" name="commit" value="Payment"></button>
+      <!-- <a href="index.html" class="button" onclick="if(confirm('Payment Successful')){return true;}else{return false;};" title="LinkTitle">
+        Make Payment
+      </a> -->
+    <!-- </div> -->
+  </form>
+</body>
+</html>
